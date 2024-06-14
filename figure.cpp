bd48f3dde7c7c772423b142463fa8e3c2bc0e80c
Kacper250998 <166056340+Kacper250998@users.noreply.github.com>
2024-06-14 16:08:57 +0200

    Update figure.cpp

--- figure.cpp
+++ figure.cpp
@@ -1,6 +1,6 @@
 #include "figure.h"
 #include <cmath>
-// Implementacja metod klasy Circle
+
 Circle::Circle(const std::string &n, double r) : name(n), radius(r) {}
 
 double Circle::getPerimeter() const {
@@ -15,7 +15,6 @@ std::string Circle::getName() const {
     return name;
 }
 
-// Implementacja metod klasy Square
 Square::Square(const std::string &n, double s) : name(n), side(s) {}
 
 double Square::getPerimeter() const {
@@ -30,7 +29,6 @@ std::string Square::getName() const {
     return name;
 }
 
-// Implementacja metod klasy Rectangle
 Rectangle::Rectangle(const std::string &n, double w, double h) : name(n), width(w), height(h) {}
 
 double Rectangle::getPerimeter() const {
